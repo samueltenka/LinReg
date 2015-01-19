c50f37c624e5b10db3eab2e4121263c7f5a9f9e2
samueltenka <iguavadon@gmail.com>
2015-01-19 18:33:57 -0500

    Update Problem_1.py
    global linear regression => 
    
    OUTPUT (note slight correlation between actual predicted values; fact that rms error < variance. :D):
    actual	  predicted
    -10.0	 0.823049548278
    -10.0	 8.58811331318
    -20.0	 -9.76659424774
    +20.0	 20.1676966607
    -20.0	 -16.4861226758
    -10.0	 -19.6092159081
    -10.0	 -16.6416522089
    +30.0	 23.3866177068
    rms error: 14.7265141179
    variance: 23.511743907591843

--- Problem_1.py
+++ Problem_1.py
@@ -4,10 +4,22 @@
 
 import CSV_Reader
 import LinearRegression
+import RMS
 
 xys = CSV_Reader.get_xys_from('train_graphs_f16_autopilot_cruise.csv')
-predict = LinearRegression.make_predictor(xys, tau=1E9, max_deg=6)
-print(predict([0.0058,-2.7,0.0,0,0.014,0.0]))
-print(predict([-0.0067,0.1,0.0,10,0.005,0.1]))
-print(predict([0.001,-3.9,-0.02,-10,0.019,0.0]))
-print(predict([0.0065,3.9,0.0,0,0.017,0.0]))
+predict = LinearRegression.make_predictor(xys, max_deg=6)
+
+print("actual\t  predicted")
+print("-10.0\t", predict([0.0058,-2.7,0.0,0,0.014,0.0]))    ## --> ~-10.0
+print("-10.0\t", predict([-0.0067,0.1,0.0,10,0.005,0.1]))   ## --> ~-10.0
+print("-20.0\t", predict([0.001,-3.9,-0.02,-10,0.019,0.0])) ## --> ~-20.0
+print("+20.0\t", predict([0.0065,3.9,0.0,0,0.017,0.0]))     ## --> ~+20.0
+print("-20.0\t", predict([0.0021,-2.1,0.0,-40,0.006,-0.1])) ## --> ~-20.0
+print("-10.0\t", predict([-0.0117,-1.6,0.0,20,0.012,-0.2])) ## --> ~-10.0
+print("-10.0\t", predict([-0.008,-2.0,0.01,-30,0.046,-0.1]))## --> ~-10.0
+print("+30.0\t", predict([0.0002,4.9,0.0,0,-0.005,0.2]))    ## --> ~+30.0
+
+
+null_model = lambda x: 0.0
+print("rms error:", RMS.compute_error(xys, predict)) ## --> positive, hopefully small number
+print("variance:", RMS.compute_error(xys, null_model)) ## --> positive number, on order of 10
