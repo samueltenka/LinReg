743a96546d8138ff5ef0e4574d9cdeb7e1cc1e7d
samueltenka <iguavadon@gmail.com>
2015-01-19 18:44:32 -0500

    Update Problem_1.py
    barring bugs, should have solved 1.1
    
    
    OUTPUT:
    
    null model's error: 23.724584113762734
    
    maximum		rms error
     degree             
    0 		 23.7609905435
    1 		 14.8233039745
    2 		 14.8342840802
    3 		 14.7445794921
    4 		 14.8149161988
    5 		 15.2434646383
    6 		 17.5029040201

--- Problem_1.py
+++ Problem_1.py
@@ -6,8 +6,29 @@ import CSV_Reader
 import LinearRegression
 import RMS
 
+train_xys = CSV_Reader.get_xys_from('train_graphs_f16_autopilot_cruise.csv')
+test_xys = CSV_Reader.get_xys_from('test_graphs_f16_autopilot_cruise.csv')
+
+predictors = [LinearRegression.make_predictor(train_xys, max_deg=i)
+              for i in range(6+1)]
+null_model = lambda x: 0.0
+
+print("null model's error:", RMS.error(test_xys, null_model)) ## not quite variance unless avg(xs)==0.0
+print()
+print("maximum\t\trms error")
+print(" degree             ")
+for i in range(0, 6+1):
+   print(i, "\t\t", RMS.error(test_xys, predictors[i]))
+
+
+
+
+'''
+## testing:
+
 xys = CSV_Reader.get_xys_from('train_graphs_f16_autopilot_cruise.csv')
-predict = LinearRegression.make_predictor(xys, max_deg=6)
+predictors = LinearRegression.make_predictor(xys, max_deg=6)
+null_model = lambda x: 0.0
 
 print("actual\t  predicted")
 print("-10.0\t", predict([0.0058,-2.7,0.0,0,0.014,0.0]))    ## --> ~-10.0
@@ -19,7 +40,6 @@ print("-10.0\t", predict([-0.0117,-1.6,0.0,20,0.012,-0.2])) ## --> ~-10.0
 print("-10.0\t", predict([-0.008,-2.0,0.01,-30,0.046,-0.1]))## --> ~-10.0
 print("+30.0\t", predict([0.0002,4.9,0.0,0,-0.005,0.2]))    ## --> ~+30.0
 
-
-null_model = lambda x: 0.0
 print("rms error:", RMS.compute_error(xys, predict)) ## --> positive, hopefully small number
 print("variance:", RMS.compute_error(xys, null_model)) ## --> positive number, on order of 10
+'''
