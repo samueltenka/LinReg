b58c1a59ca1d6d6d05a9f64d3a435a6950ee50c3
samueltenka <iguavadon@gmail.com>
2015-01-19 17:57:25 -0500

    Update Predictor.py
    SYNTAX CHANGED:
    Now ``make_predict" doesn't take in ``target_x", but instead returns a function that takes it in.
    If tau not specified, it does global linear regression, avoiding the recomputation of ``weights" for each ``target_x".
    (see test examples.)
    
    OUTPUT REMAINS GOOD:
    For max_deg==3: 1.6
    For max_deg==4: 8.8136861622

--- Predictor.py
+++ Predictor.py
@@ -7,39 +7,48 @@ from numpy.linalg import *
 from numpy import *
 from math import *
 
-def closeness(point, center, tau):
-   diff = point-center
-   mag_diff = dot(diff, diff)[0][0]
-   return exp(-mag_diff/(2*tau))
-
 def get_feats(point, max_deg):
    return [1] + [coor**d for coor in point for d in range(1, max_deg+1)]
+
+def make_kernel(center, beta):
+   def kernel(point):
+      diff = point-center
+      return exp(-beta*dot(diff, diff)/2)
+   return kernel
    
-def predict(xys, target_x, tau=1000.0, max_deg=3):
-   '''e.g.  if xys==[([0.0], 1.0), ([2.0], 2.0), ([4.0], 3.0), ([6.0], 4.0)]
-      and target_x==[1.2], then should return (1.2, 1.6);
-	  tau controls duration of context; if tau is small, it'll guess based
-      on closest datapoints, at risk of overfitting; if tau is large, it'll
-      take into account the whole history, at risk of oversimplifying.'''
-   ## everybody's a 2d array, even vectors and scalars (e.g. see return)
-   target_x = array([[feat] for feat in get_feats(target_x, max_deg)])
+def make_predictor(xys, max_deg, tau=None):
+   '''Now ``make_predict" doesn't take in ``target_x",
+      but instead returns ``predict" that eats ``target_x".
+      If tau not specified, ``predict" does global linear regression,
+      avoiding the recomputation of ``weights" for each ``target_x".'''
    xs = array([get_feats(x, max_deg) for (x, y) in xys])
    ys = array([[y] for (x, y) in xys])
    
-   weight_func = lambda x: closeness(x, target_x, tau)
-   xs_t_weighted = transpose(array([x*weight_func(x) for x in  xs]))
-   pseudo_inverse = dot(inv(dot(xs_t_weighted, xs)), xs_t_weighted)
+   def compute_weights(weight_func):
+      xs_t_weighted = transpose(array([x*weight_func(x) for x in xs]))
+      pseudo_inverse = dot(inv(dot(xs_t_weighted, xs)), xs_t_weighted)
+      return dot(pseudo_inverse, ys)
+      
+   if tau:
+      get_weight = lambda target_x: \
+                   compute_weights(make_kernel(target_x, beta=1.0/tau))
+   else:
+      weights = compute_weights(make_kernel(xs[0], beta=0.0))
+      get_weight = lambda target_x: weights
+   
+   def predict(target_x):
+      target_x = array([[feat] for feat in get_feats(target_x, max_deg)])
+      weights = get_weight(target_x)
+      return dot(transpose(weights), target_x)[0][0]
 
-   weights = dot(pseudo_inverse, ys)
-   return dot(transpose(weights), target_x)[0][0]
+   return predict
+      
 
 
 ## testing:
 print("For max_deg==3:",
-      predict([([0.0], 1.0), ([2.0], 2.0), ([4.0], 3.0), ([6.0], 4.0)],
-              [1.2],
-              max_deg=3)) ## --> 1.6
+      make_predictor([([0.0], 1.0), ([2.0], 2.0), ([4.0], 3.0), ([6.0], 4.0)],
+              max_deg=3)([1.2])) ## --> 1.6
 print("For max_deg==4:",
-      predict([([0.0], 1.0), ([2.0], 2.0), ([4.0], 3.0), ([6.0], 4.0)],
-              [1.2],
-              max_deg=4)) ## --> not 1.6
+      make_predictor([([0.0], 1.0), ([2.0], 2.0), ([4.0], 3.0), ([6.0], 4.0)],
+              max_deg=4)([1.2])) ## --> not 1.6
