167748f4070a8266c3ebe31f869f5d3dabcb9caa
samueltenka <iguavadon@gmail.com>
2015-01-19 19:40:32 -0500

    Update Problem_1.py
    Sweeps to find best regularization parameter.
    
    OUTPUT:
    (1, -40)  (1, -39)  (1, -38)  (1, -37)  (1, -36)  (1, -35)  (1, -34)  (1, -33)  (1, -32)  (1, -31)  (1, -30)  (1, -29)  (1, -28)  (1, -27)  (1, -26)  (1, -25)  (1, -24)  (1, -23)  (1, -22)  (1, -21)  (1, -20)  (1, -19)  (1, -18)  (1, -17)  (1, -16)  (1, -15)  (1, -14)  (1, -13)  (1, -12)  (1, -11)  (1, -10)  (1, -9)  (1, -8)  (1, -7)  (1, -6)  (1, -5)  (1, -4)  (1, -3)  (1, -2)  (1, -1)  (1, 0)  (1, 1)  (1, 2)  (1, 3)  (1, 4)  (1, 5)  (1, 6)  (1, 7)  (1, 8)  (1, 9)  (1, 10)  (1, 11)  (1, 12)  (1, 13)  (1, 14)  (1, 15)  (1, 16)  (1, 17)  (1, 18)  (1, 19)  (1, 20)  
     14.3006464138 
    
    
    (2, -40)  (2, -39)  (2, -38)  (2, -37)  (2, -36)  (2, -35)  (2, -34)  (2, -33)  (2, -32)  (2, -31)  (2, -30)  (2, -29)  (2, -28)  (2, -27)  (2, -26)  (2, -25)  (2, -24)  (2, -23)  (2, -22)  (2, -21)  (2, -20)  (2, -19)  (2, -18)  (2, -17)  (2, -16)  (2, -15)  (2, -14)  (2, -13)  (2, -12)  (2, -11)  (2, -10)  (2, -9)  (2, -8)  (2, -7)  (2, -6)  (2, -5)  (2, -4)  (2, -3)  (2, -2)  (2, -1)  (2, 0)  (2, 1)  (2, 2)  (2, 3)  (2, 4)  (2, 5)  (2, 6)  (2, 7)  (2, 8)  (2, 9)  (2, 10)  (2, 11)  (2, 12)  (2, 13)  (2, 14)  (2, 15)  (2, 16)  (2, 17)  (2, 18)  (2, 19)  (2, 20)  
     14.224947606 
    
    
    (3, -40)  (3, -39)  (3, -38)  (3, -37)  (3, -36)  (3, -35)  (3, -34)  (3, -33)  (3, -32)  (3, -31)  (3, -30)  (3, -29)  (3, -28)  (3, -27)  (3, -26)  (3, -25)  (3, -24)  (3, -23)  (3, -22)  (3, -21)  (3, -20)  (3, -19)  (3, -18)  (3, -17)  (3, -16)  (3, -15)  (3, -14)  (3, -13)  (3, -12)  (3, -11)  (3, -10)  (3, -9)  (3, -8)  (3, -7)  (3, -6)  (3, -5)  (3, -4)  (3, -3)  (3, -2)  (3, -1)  (3, 0)  (3, 1)  (3, 2)  (3, 3)  (3, 4)  (3, 5)  (3, 6)  (3, 7)  (3, 8)  (3, 9)  (3, 10)  (3, 11)  (3, 12)  (3, 13)  (3, 14)  (3, 15)  (3, 16)  (3, 17)  (3, 18)  (3, 19)  (3, 20)  
     14.1522551247 
    
    
    (4, -40)  (4, -39)  (4, -38)  (4, -37)  (4, -36)  (4, -35)  (4, -34)  (4, -33)  (4, -32)  (4, -31)  (4, -30)  (4, -29)  (4, -28)  (4, -27)  (4, -26)  (4, -25)  (4, -24)  (4, -23)  (4, -22)  (4, -21)  (4, -20)  (4, -19)  (4, -18)  (4, -17)  (4, -16)  (4, -15)  (4, -14)  (4, -13)  (4, -12)  (4, -11)  (4, -10)  (4, -9)  (4, -8)  (4, -7)  (4, -6)  (4, -5)  (4, -4)  (4, -3)  (4, -2)  (4, -1)  (4, 0)  (4, 1)  (4, 2)  (4, 3)  (4, 4)  (4, 5)  (4, 6)  (4, 7)  (4, 8)  (4, 9)  (4, 10)  (4, 11)  (4, 12)  (4, 13)  (4, 14)  (4, 15)  (4, 16)  (4, 17)  (4, 18)  (4, 19)  (4, 20)  
     14.1295466929 
    
    
    (5, -40)  (5, -39)  (5, -38)  (5, -37)  (5, -36)  (5, -35)  (5, -34)  (5, -33)  (5, -32)  (5, -31)  (5, -30)  (5, -29)  (5, -28)  (5, -27)  (5, -26)  (5, -25)  (5, -24)  (5, -23)  (5, -22)  (5, -21)  (5, -20)  (5, -19)  (5, -18)  (5, -17)  (5, -16)  (5, -15)  (5, -14)  (5, -13)  (5, -12)  (5, -11)  (5, -10)  (5, -9)  (5, -8)  (5, -7)  (5, -6)  (5, -5)  (5, -4)  (5, -3)  (5, -2)  (5, -1)  (5, 0)  (5, 1)  (5, 2)  (5, 3)  (5, 4)  (5, 5)  (5, 6)  (5, 7)  (5, 8)  (5, 9)  (5, 10)  (5, 11)  (5, 12)  (5, 13)  (5, 14)  (5, 15)  (5, 16)  (5, 17)  (5, 18)  (5, 19)  (5, 20)  
     14.0654420679 
    
    
    (6, -40)  (6, -39)  (6, -38)  (6, -37)  (6, -36)  (6, -35)  (6, -34)  (6, -33)  (6, -32)  (6, -31)  (6, -30)  (6, -29)  (6, -28)  (6, -27)  (6, -26)  (6, -25)  (6, -24)  (6, -23)  (6, -22)  (6, -21)  (6, -20)  (6, -19)  (6, -18)  (6, -17)  (6, -16)  (6, -15)  (6, -14)  (6, -13)  (6, -12)  (6, -11)  (6, -10)  (6, -9)  (6, -8)  (6, -7)  (6, -6)  (6, -5)  (6, -4)  (6, -3)  (6, -2)  (6, -1)  (6, 0)  (6, 1)  (6, 2)  (6, 3)  (6, 4)  (6, 5)  (6, 6)  (6, 7)  (6, 8)  (6, 9)  (6, 10)  (6, 11)  (6, 12)  (6, 13)  (6, 14)  (6, 15)  (6, 16)  (6, 17)  (6, 18)  (6, 19)  (6, 20)  
     14.0252673121 
    
    
    null model's error: 23.724584113762734
    
    maximum		rms error
     degree             
    1 		 14.8233039745
    2 		 14.8342840801
    3 		 14.7445794916
    4 		 14.8149176885
    5 		 15.2414605135
    6 		 17.3111797251

--- Problem_1.py
+++ Problem_1.py
@@ -2,44 +2,52 @@
    Creation 2015 by Samuel Tenka
    .............................'''
 
-import CSV_Reader
-import LinearRegression
+from CSV_Reader import get_xys_from
+from LinearRegression import make_predictor
 import RMS
+from math import exp
+from random import random
 
-train_xys = CSV_Reader.get_xys_from('train_graphs_f16_autopilot_cruise.csv')
-test_xys = CSV_Reader.get_xys_from('test_graphs_f16_autopilot_cruise.csv')
-
-predictors = [LinearRegression.make_predictor(train_xys, max_deg=i)
-              for i in range(6+1)]
+train_xys = get_xys_from('train_graphs_f16_autopilot_cruise.csv')
+test_xys = get_xys_from('test_graphs_f16_autopilot_cruise.csv')
+train_error_of = lambda predictor: RMS.error(train_xys, predictor)
+test_error_of = lambda predictor: RMS.error(test_xys, predictor)
 null_model = lambda x: 0.0
 
-print("null model's error:", RMS.error(test_xys, null_model)) ## not quite variance unless avg(xs)==0.0
+def generate_predictors(max_deg):
+   for log_lambda in range(-40, 20+1):
+      print((max_deg, log_lambda), end="  ")
+      pred = make_predictor(train_xys, max_deg=max_deg,
+                            reg_param=exp(log_lambda))
+      yield pred
+def get_predictor(max_deg):
+   rtrn = min((train_error_of(pred), random(), pred)
+              for pred in generate_predictors(max_deg))
+   #rtrn = (error_of(null_model), null_model)
+   #for pred in generate_predictors(max_deg):
+   #   error = train_error_of(pred)
+   #   if error < rtrn[0]:
+   #      rtrn = (error, pred)
+   print("\n", rtrn[0], "\n\n")
+   return rtrn[2]
+
+predictors = [null_model] + [get_predictor(i) for i in range(1, 6+1)]
+
+print("null model's error:", test_error_of(null_model))
 print()
 print("maximum\t\trms error")
 print(" degree             ")
-for i in range(0, 6+1):
-   print(i, "\t\t", RMS.error(test_xys, predictors[i]))
-
-
+for i in range(1, 6+1):
+   print(i, "\t\t", test_error_of(predictors[i]))
 
 
 '''
-## testing:
-
-xys = CSV_Reader.get_xys_from('train_graphs_f16_autopilot_cruise.csv')
-predictors = LinearRegression.make_predictor(xys, max_deg=6)
-null_model = lambda x: 0.0
-
-print("actual\t  predicted")
-print("-10.0\t", predict([0.0058,-2.7,0.0,0,0.014,0.0]))    ## --> ~-10.0
-print("-10.0\t", predict([-0.0067,0.1,0.0,10,0.005,0.1]))   ## --> ~-10.0
-print("-20.0\t", predict([0.001,-3.9,-0.02,-10,0.019,0.0])) ## --> ~-20.0
-print("+20.0\t", predict([0.0065,3.9,0.0,0,0.017,0.0]))     ## --> ~+20.0
-print("-20.0\t", predict([0.0021,-2.1,0.0,-40,0.006,-0.1])) ## --> ~-20.0
-print("-10.0\t", predict([-0.0117,-1.6,0.0,20,0.012,-0.2])) ## --> ~-10.0
-print("-10.0\t", predict([-0.008,-2.0,0.01,-30,0.046,-0.1]))## --> ~-10.0
-print("+30.0\t", predict([0.0002,4.9,0.0,0,-0.005,0.2]))    ## --> ~+30.0
-
-print("rms error:", RMS.compute_error(xys, predict)) ## --> positive, hopefully small number
-print("variance:", RMS.compute_error(xys, null_model)) ## --> positive number, on order of 10
+maximum		rms error
+ degree             
+1 		 14.8112273159
+2 		 14.8342486623
+3 		 14.7246665024
+4 		 14.8121911411
+5 		 14.8784538269
+6 		 14.9338347157
 '''
