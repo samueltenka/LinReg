f80e89ff80efdcb797c09f1622e4dfd7807accdc
samueltenka <iguavadon@gmail.com>
2015-01-19 21:33:27 -0500

    Update CSV_Reader.py
    Allowed for reading of csv's with no y's (e.g. Kaggle test data)

--- CSV_Reader.py
+++ CSV_Reader.py
@@ -1,6 +1,14 @@
+'''.............................
+   Creation 2015 by Samuel Tenka
+   .............................'''
+''' CSV reader of files of form:
+---HEADING LINE
+---DATA LINES of form
+------ID,x1,x2,x3,...,xn,y'''
+
 def get_row(file):
    return file.readline().split(',')[1:] ## neglect first val (ID#).
-def get_xys_from(filename):
+def get_xys_from(filename, include_ys=True):
    ''' return format [((x1,...,xn), y),
                       ((x1,...,xn), y),...]'''
    xys = []
@@ -8,12 +16,20 @@ def get_xys_from(filename):
       f.readline() ## headings, to be discarded
       for row in iter(lambda:get_row(f), []):
          values = [eval(s) for s in row]
-         xys.append((values[:-1], values[-1]))
+         xy = (values[:-1], values[-1]) if include_ys else \
+              (values, None)
+         xys.append(xy)
    return xys
 
 
+'''
 ## testing:
 g = get_xys_from('train_graphs_f16_autopilot_cruise.csv')
-print(g[0]) # -->([0.0058, -2.7, 0.0, 0, 0.014, 0.0], -10.0)
-print(g[-1]) # --> ([0.0009, 8.7, 0.0, -50, -0.009000000000000001, 0.1], 20.0)
-print(len(g)) # --> 3426
+print(g[0])  ## --> ([0.0058, -2.7, 0.0, 0, 0.014, 0.0], -10.0)
+print(g[-1]) ## --> ([0.0009, 8.7, 0.0, -50, -0.009000000000000001, 0.1], 20.0)
+print(len(g))## --> 3426
+
+h = get_xys_from('test_f16_autopilot_cruise.csv', include_ys = False)
+print(h[0])  ## --> ([-0.0069, 1.8, 0.0, -70, -0.008, 0.1], None)
+
+'''
