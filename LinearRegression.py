a2fab1db5e0485dfacf0274e3dc1f37736d3ada0
samueltenka <iguavadon@gmail.com>
2015-01-19 19:11:06 -0500

    Update LinearRegression.py
    L2 regularization now possible, with optional parameter inputted to make_predictor.
    (features remain unnormalized.)

--- LinearRegression.py
+++ LinearRegression.py
@@ -16,7 +16,7 @@ def make_kernel(center, beta):
       return exp(-beta*dot(diff, diff)/2)
    return kernel
    
-def make_predictor(xys, max_deg, tau=None):
+def make_predictor(xys, max_deg, tau=None, reg_param=0.0):
    '''Now ``make_predict" doesn't take in ``target_x",
       but instead returns ``predict" that eats ``target_x".
       If tau not specified, ``predict" does global linear regression,
@@ -26,7 +26,8 @@ def make_predictor(xys, max_deg, tau=None):
    
    def compute_weights(weight_func):
       xs_t_weighted = transpose(array([x*weight_func(x) for x in xs]))
-      pseudo_inverse = dot(inv(dot(xs_t_weighted, xs)), xs_t_weighted)
+      regularizer = reg_param * identity(len(xs[0]))
+      pseudo_inverse = dot(inv(regularizer + dot(xs_t_weighted, xs)), xs_t_weighted)
       return dot(pseudo_inverse, ys)
       
    if tau:
@@ -44,6 +45,7 @@ def make_predictor(xys, max_deg, tau=None):
    return predict
       
 
+
 ## testing:
 print("For max_deg==3:",
       make_predictor([([0.0], 1.0), ([2.0], 2.0), ([4.0], 3.0), ([6.0], 4.0)],
@@ -51,3 +53,4 @@ print("For max_deg==3:",
 print("For max_deg==4:",
       make_predictor([([0.0], 1.0), ([2.0], 2.0), ([4.0], 3.0), ([6.0], 4.0)],
               max_deg=4)([1.2])) ## --> not 1.6
+
